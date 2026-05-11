ec14538e4aaffeae0d25f4bed9a00bfa9c7e8bb3
Winston Fassett <dev@winstonfassett.com>
2026-05-11 08:04:01 -0500

    chore: remove amp backend (Amp went paid), close dead tickets

--- server/backends/__init__.py
+++ server/backends/__init__.py
@@ -1,13 +1,11 @@
 """Backend Services for Voice Bot"""
 
-from .amp import AmpLLMService
 from .claude_code import ClaudeCodeLLMService
 from .moltis import MoltisLLMService
 from .openclaw import OpenClawLLMService, OpenClawVoiceLLMService
 from .pi_rpc import PiRPCLLMService
 
 __all__ = [
-    "AmpLLMService",
     "ClaudeCodeLLMService",
     "MoltisLLMService",
     "OpenClawLLMService",
